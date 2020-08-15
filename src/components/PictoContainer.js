cdf07cd62d73ab795535fca891c845311ef87f7e
Mikkel Gade <magde05@gmail.com>
2020-08-15 09:44:05 +0200

    Refactored PictoContainer to function component

--- src/components/PictoContainer.js
+++ src/components/PictoContainer.js
@@ -1,77 +1,75 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Pictogram from "./Pictogram";
 import PictoAdder from "./PictoAdder";
 import { v4 as uuid } from "uuid";
 import { Grid } from "@material-ui/core";
 import { PictoData } from "../data/data";
 
-export default class PictoContainer extends Component {
-    state = {
+export default function PictoContainer(props) {
+    const [state, setState] = useState({
         pictograms: PictoData,
+    });
+
+    const handleDelete = id => {
+        const pictograms = state.pictograms.filter(p => p.id !== id);
+        setState({ pictograms });
     };
 
-    handleDelete = id => {
-        const pictograms = this.state.pictograms.filter(p => p.id !== id);
-        this.setState({ pictograms });
-    };
-
-    handleUp = id => {
+    const handleUp = id => {
         console.log("Up", id);
-        let pictograms = this.state.pictograms;
+        let pictograms = state.pictograms;
         const i = pictograms.findIndex(p => p.id === id);
         if (i > 0) {
-            const swap = this.getSwapper(pictograms);
+            const swap = getSwapper(pictograms);
             pictograms = swap(i, i - 1);
-            this.setState({ pictograms });
+            setState({ pictograms });
         }
     };
 
-    handleDown = id => {
+    const handleDown = id => {
         console.log("Down", id);
-        let pictograms = this.state.pictograms;
+        let pictograms = state.pictograms;
         const i = pictograms.findIndex(p => p.id === id);
         if (i < pictograms.length - 1) {
-            const swap = this.getSwapper(pictograms);
+            const swap = getSwapper(pictograms);
             pictograms = swap(i, i + 1);
-            this.setState({ pictograms });
+            setState({ pictograms });
         }
     };
 
-    getSwapper = ([...arr]) => (x, y) =>
+    const getSwapper = ([...arr]) => (x, y) =>
         arr.length > 1 ? (([arr[x], arr[y]] = [arr[y], arr[x]]), arr) : arr;
 
-    handleAdd = () => {
+    const handleAdd = () => {
         const newPic = {
             id: uuid(),
             img: "https://image.flaticon.com/icons/svg/2912/2912288.svg",
             text: "Take picture",
         };
-        const pictograms = [...this.state.pictograms, newPic];
-        this.setState({ pictograms });
+        const pictograms = [...state.pictograms, newPic];
+        setState({ pictograms });
     };
 
-    render() {
-        const { pictograms } = this.state;
-        return (
-            <Grid
-                container
-                direction="column"
-                justify="flex-start"
-                alignItems="center"
-                spacing={2}
-            >
-                {pictograms.map(p => (
-                    <Grid item key={p.id}>
-                        <Pictogram
-                            data={p}
-                            onDelete={this.handleDelete}
-                            onDown={this.handleDown}
-                            onUp={this.handleUp}
-                        ></Pictogram>
-                    </Grid>
-                ))}
-                <PictoAdder onAdd={this.handleAdd}></PictoAdder>
-            </Grid>
-        );
-    }
+    const { pictograms } = state;
+    return (
+        <Grid
+            container
+            direction="column"
+            justify="flex-start"
+            alignItems="center"
+            spacing={2}
+        >
+            {pictograms.map(p => (
+                <Grid item key={p.id}>
+                    <Pictogram
+                        data={p}
+                        onDelete={handleDelete}
+                        onDown={handleDown}
+                        onUp={handleUp}
+                    ></Pictogram>
+                </Grid>
+            ))}
+            <PictoAdder onAdd={handleAdd}></PictoAdder>
+        </Grid>
+    );
 }

e1edb8bacc78df5eebdd1a92b39a97a5f0779099
Mikkel Gade <mg@cego.dk>
2020-09-23 07:56:05 +0200

    Started adding more to settings

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -1,3 +1,4 @@
+import { Button } from "@material-ui/core";
 import React from "react";
 import PictogramBase from "../components/PictogramBase";
 
@@ -5,7 +6,24 @@ export default function Settings() {
     return (
         <PictogramBase>
             <h1>Settings</h1>
-            <p>Coming soon...</p>
+            <h2>Search</h2>
+            <ul>
+                <li>Preferred style - TODO</li>
+                <li>Number of results - TODO</li>
+            </ul>
+
+            <h2>Danger Zone</h2>
+            <ul>
+                <li>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => localStorage.removeItem("pictograms")}
+                    >
+                        Clear pictograms
+                    </Button>
+                </li>
+            </ul>
         </PictogramBase>
     );
 }

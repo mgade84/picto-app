b84647a03d91a2c5bce2ad2c5c340c455c4909c8
Mikkel Gade <mgade05@gmail.com>
2020-12-12 15:10:39 +0100

    Added initial login page

---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyledFirebaseAuth } from "react-firebaseui";
+import "firebase/auth";
+
+export default function LoginPage() {
+    const uiConfig = {
+        firebase,
+    };
+
+    return <StyledFirebaseAuth></StyledFirebaseAuth>;
+}
